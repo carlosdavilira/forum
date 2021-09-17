cad365db7dbfe40559a09121f044d456a517f0e4
Carlos David <carlosdavilira@gmail.com>
2021-09-17 02:25:12 -0300

    [Forum][MOD] Improving pagination estrategy

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,8 +46,10 @@ public class TopicosController {
 	@GetMapping
 	public Page<TopicoDto> lista(
 			@RequestParam(required = false) String nomeCurso,
-			@RequestParam int pagina, int qtd) {
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+			@PageableDefault(sort="id", direction = Direction.DESC, page = 0, size = 10)Pageable paginacao) {
+		
+		//Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
+		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos );
