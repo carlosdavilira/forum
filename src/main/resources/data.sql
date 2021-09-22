6fc4124f2028d62d0d2ba5e2e9dad96b4c7c5aa2
Carlos David <carlosdavilira@gmail.com>
2021-09-22 02:32:53 -0300

    [Forum][ADD] Add requests policies and enabling token security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$WP16uCAK537RGS.WiOTl0.7C0yWhWrXbML6zqFbGjSr3maWnLlz/6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
